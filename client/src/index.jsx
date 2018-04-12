3c686d2870ef8d5270abba4feae54804cde1cbe6
miriam30 <miriam.alvarez.p96@gmail.com>
2018-04-11 20:16:00 -0500

    basic style added for header and about us

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter, Route } from "react-router-dom";
 import PrimaryPage from './PrimaryPage.jsx'
 
+/*Components */
+import Footer from '../src/components/base/footer.jsx'
+import HeaderBar from '../src/components/base/menu.jsx'
 
 
 class App extends React.Component {
