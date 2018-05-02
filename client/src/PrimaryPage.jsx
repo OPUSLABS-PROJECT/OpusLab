3161aae9ea56d3afb522e310493e9d45a04fea2f
Edward060290 <barranoneduardo@gmail.com>
2018-05-02 12:45:42 -0500

    all rendering

--- client/src/PrimaryPage.jsx
+++ client/src/PrimaryPage.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './components/HomePage/Home.jsx';
-import Project from './components/HomePage/ProjectsMain.jsx';
-import Contact from './components/HomePage/Contact.jsx'
+
 const PrimaryPage = ({ match }) => (
   <div className="primary-page">
     <main>
       <Switch>
         <Route exact path={`${match.path}`} component={Home}/>
-        <Route path={`${match.url}project`} component={Project} />
-        <Route path={`${match.url}contact`} component={Contact} />
       </Switch>
     </main>
   </div>
