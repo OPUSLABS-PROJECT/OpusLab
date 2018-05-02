9c6aeabce426e8869dd17e2b2e4a48705542e5df
Heber <hebergutierrez.p@gmail.com>
2018-05-02 08:36:07 +0000

    opuslab-landingpage

--- client/src/component/Video.jsx
+++ client/src/component/Video.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Video = () => (
 	<div>
 	 <div id='video'>
-		 <iframe src="https://player.vimeo.com/video/224973299?autoplay=1&loop=1&title=0&byline=0&portrait=0" width="640" height="360" align="center"></iframe>
+		 <iframe src="https://player.vimeo.com/video/266384010?autoplay=1&loop=1&title=0&byline=0&portrait=0" width="640" height="360" align="center"></iframe>
 	 </div>
 
  </div>
